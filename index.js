73d7b73dde26288ceaccd81108fc60df3adc3d40
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-24 09:56:53 +0500

    add basic implementation

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-export default item => item;
+import { text, html, matchRemoveList } from 'commonmark-helpers';
+import { is, map } from 'ramda';
+
+const getMatcher = item => {
+  if (is(Function, item)) {
+    return item;
+  }
+
+  if (is(RegExp, item)) {
+    return node => text(node).match(item);
+  }
+};
+
+const result = node => ({
+  text: text(node),
+  html: html(node),
+  node
+});
+
+export default (removeList, input) =>
+  result(matchRemoveList(input, ...map(getMatcher, removeList)));
--- test.js
+++ test.js
@@ -1,6 +1,36 @@
 import { equal } from 'assert';
-import getMdContent from './index';
+import { isHeader } from 'commonmark-helpers';
+import getContent from './index';
 
-it('should getMdContent', () => {
-  equal(getMdContent('unicorns'), 'unicorns');
+// что такое контент?
+// это статья без нодs заголовка и ноды c датой
+
+const simple = `
+# h1
+
+content
+`.trim();
+
+const medium = `
+# h1
+
+## h2
+
+content
+`.trim();
+
+it('should simple getContent matcher', () => {
+  equal(getContent([isHeader], simple).text, 'content');
+});
+
+it('should simple getContent RexExp', () => {
+  equal(getContent([/h1/], simple).text, 'content');
+});
+
+it('should simple getContent full list', () => {
+  equal(getContent([/h1/, /h2/], medium).text, 'content');
+});
+
+it('should simple getContent empty list', () => {
+  equal(getContent([], `content`).text, 'content');
 });
